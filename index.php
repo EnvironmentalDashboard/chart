230b998e1258fddcf8697a7d6fe25de12121a59a
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-12-14 11:01:43 -0500

    off by default

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $compare = false;
 $inverted = false;
 $start = 0; // if set by user, $min will be set to this
 $time_frame = 'day';
-$cleveland = true;
+$cleveland = false;
 extract($_GET, EXTR_IF_EXISTS); // imports GET array into the current symbol table (i.e. makes each entry of GET a variable) if the variable already exists
 $colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#7fc97f', '#beaed4', '#fdc086', '#ffff99', '#386cb0', '#f0027f', '#bf5b17', '#666666', '#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3', '#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'];
 for ($i=0; $i < $charts; $i++) { 
